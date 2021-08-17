e1218d3fb2e934a9d278d01a02e76a48cde820ea
ozden <mehmetozden1981@gamil.com>
2021-08-17 16:34:25 -0400

    using redux...

--- src/index.js
+++ src/index.js
@@ -3,10 +3,69 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducer from './reducers/index';
+import {Provider} from 'react-redux';
+
+const store= createStore(allReducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   );
+
+//let store=createStore(reducer)
+
+//STORE
+//ACTION
+//REDUCER
+//DISPATCH
+
+
+// const increment =()=>{
+//   return{
+
+//     type:'INCREMENT'
+
+//   };
+// };
+
+// const decrement=()=>{
+//   return{
+
+//     type:'DECREMENT'
+
+//   };
+// };
+
+// const counter=(state=0, action)=>{
+//   switch(action.type){
+//     case "INCREMENT":
+//       return state +1;
+
+//     case "DECREMENT":
+//       return state-1;
+//   }
+// };
+
+// let store =createStore(counter);
+
+// //Display
+
+// store.subscribe(() => console.log(store.getState()));
+
+// //Dispatch
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+ 
+  <React.StrictMode> 
+    <Provider store={store}>
     <App />
+    
+    </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
